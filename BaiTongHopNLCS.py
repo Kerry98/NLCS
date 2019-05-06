855d6ac58e62801c23e3362e07a21da6263e9ebe
Kerry98 <42793132+Kerry98@users.noreply.github.com>
2019-05-06 21:08:43 +0700

    Update BaiTongHopNLCS.py

--- BaiTongHopNLCS.py
+++ BaiTongHopNLCS.py
@@ -29,8 +29,9 @@ def openfn():
 #xu ly
 def process(imgPath):
 	global panel2
-	#doc hinh
+	#doc anh mau (anh xam = 0)
 	img = cv2.imread(imgPath,1)
+	
 	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 	blur = cv2.GaussianBlur(gray,(5,5),0)
 	edged = cv2.Canny(blur,75,200)
